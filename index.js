b81007267ecc73b9777eddce983918e35edcbc1f
Joel Sequeira <joelseq96@gmail.com>
2017-10-21 04:34:25 -0700

    Final Code

--- index.js
+++ index.js
@@ -36,7 +36,104 @@ app.set('view engine', 'ejs');
 // Update   |   /:id      |   PUT       | Update a particular post
 // Destroy  |   /:id      |   DELETE    | Delete a particular post
 // =======================================================================
+app.get('/', (req, res) => {
+  db.allDocs({
+    include_docs: true,
+    attachments: true
+  }, function(error, result) {
+    if(error) {
+      console.log(error);
+    } else {
+      res.render('posts', { posts: result.rows });
+    }
+  });
+});
 
+app.get('/new', (req, res) => {
+  res.render('new-post');
+});
+
+app.post('/', (req, res) => {
+  var toSave = {
+    title: req.body.title,
+    post: req.body.post
+  };
+  
+  db.post(toSave, function(error, posted) {
+    if(error) {
+      console.log(error);
+    } else {
+      console.log(posted);
+    }
+    res.redirect('/');
+  });
+});
+
+app.get('/:id', (req, res) => {
+  db.get(req.params.id, function(error, found) {
+    if(error) {
+      console.log(error);
+      res.redirect('/');
+    } else {
+      res.render('show-post', { post: found });
+    }
+  });
+});
+
+app.get('/:id/edit', (req, res) => {
+  db.get(req.params.id, function(error, found) {
+    if(error) {
+      console.log(error);
+      res.redirect('/');
+    } else {
+      res.render('edit-post', { post: found });
+    }
+  });
+});
+
+app.put('/:id', (req, res) => {
+  var toSave = {
+    title: req.body.title,
+    post: req.body.post
+  };
+  
+  db.get(req.params.id, function(error, found) {
+    if(error) {
+      console.log(error);
+      res.redirect('/');
+    } else {
+      db.put({
+        _id: found._id,
+        _rev: found._rev,
+        title: req.body.title,
+        post: req.body.post
+      }, function(error, saved) {
+        if(error) {
+          console.log(error);
+        }
+        res.redirect('/');
+      });
+    }
+  });
+});
+
+app.delete('/:id', (req, res) => {
+  db.get(req.params.id, function(err, found) {
+    if(err) {
+      console.log(err);
+      res.redirect('/');
+    } else {
+      db.remove(found, function(err, removed) {
+        if(err) {
+          console.log(err);
+          res.redirect('/');
+        } else {
+          res.redirect('/');
+        }
+      });
+    }
+  });
+});
 
 // Listen for requests
 app.listen(PORT, () => {
